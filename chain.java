b906b6ffac8df5bdef20f68e7deac16f1e4b88ba
FizzBuzzVPS <trentwilliamjones@gmail.com>
2012-11-14 13:04:26 +0800

    Calculation summary & more verbose output added

--- chain.java
+++ chain.java
@@ -1,7 +1,7 @@
 /*********************************************************
  * Author:  Trent Jones                                  *
- * Version: 1.0 - Basic functionality			 *
- * Date:    12/11/12					 *
+ * Version: 1.1 - Added calculations output		 *
+ * Date:    13/11/12					 *
  * Summary: Calculate optimal parenthesization for 	 *
  * 	    matrix multplication                         *
  *********************************************************/
@@ -13,7 +13,7 @@ public class chain
 {
 	public static void main(String[] args)
 	{
-		boolean verbose = true; // Verbose output on/off, manual only. TODO: Convert to command line option.
+		boolean verbose = false; // Verbose output on/off, manual only. TODO: Convert to command line option.
 		int iMatriceCount = 0; // Number of matrices
 		ArrayList<Matrix> mDimensions = new ArrayList<Matrix>(); // List of matrice dimensions
 		ArrayList<String> parentheses = new ArrayList<String>(); // List rep of matrices. Use list to easily insert parentheses.
@@ -32,9 +32,7 @@ public class chain
 
 		// Only print if verbose is on (-v)
 		if (verbose)
-		{
 			System.out.println("\nVerbose output is on.\n");
-		}
 		
 		// Receive dimensions
 		for (int i = 0; i < iMatriceCount; i++)
@@ -47,10 +45,14 @@ public class chain
 		}
 
 		// Loop through and find highest value
+		int iCalculations = 0; // Track number of calculations needed for this optimisation.
+		int iHighVal = -1; // Used to find highest multiplication.
+		int iMatrix = -1; // Track which matrix has the highest columns for multiplications.
+		int iLoopCount = 0;
 		while (iMatriceCount > 1)
 		{
-			int iHighVal = -1; // Just for comparison.
-			int iMatrix = -1; // Matrix to work paren. around.
+			iHighVal = -1; // Best to reset at the start of each loop
+			iMatrix = -1; // Again, best to reset this.
 			for (int i = 0; i < iMatriceCount - 1; i++) // Check columns of all except last to find highest value
 			{
 				int columns = mDimensions.get(i).getColumns();
@@ -64,15 +66,24 @@ public class chain
 			// Now add parentheses around relevant matrix.
 			parentheses.set(iMatrix, "(" + parentheses.get(iMatrix) + parentheses.get(iMatrix + 1) + ")");
 			parentheses.remove(iMatrix + 1);
+			if (verbose)
+				System.out.println("\n\tStep " + iLoopCount + " parenthesization: " + getString(parentheses));
+
+			// Figure out needed calculations
+			iCalculations += (mDimensions.get(iMatrix).getRows() * mDimensions.get(iMatrix).getColumns() * mDimensions.get(iMatrix + 1).getColumns());
+			if (verbose)
+				System.out.println("\tStep " + iLoopCount + " calculations: " + iCalculations);
 	
 			// reset list to represent available multiplication possibilities
 			iMatriceCount--;
-			int tempCols = mDimensions.get(iMatrix + 1).getColumns();
+			mDimensions.get(iMatrix).setColumns(mDimensions.get(iMatrix + 1).getColumns());
 			mDimensions.remove(iMatrix + 1);
-			mDimensions.get(iMatrix).setColumns(tempCols);
+
+			iLoopCount++;
 		}
 
-		System.out.println("Result: " + getString(parentheses));
+		System.out.println("\nPartially optimised parentheses: " + getString(parentheses));
+		System.out.println("Calculations needed: " + iCalculations);
 	}
 
 	/********************************************
